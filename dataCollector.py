1a618cc9fd4ef3d67a4fda40e92424607717338c
Kunal Dutta <kdutta@berkeley.edu>
2019-10-14 23:09:30 -0700

    moved files into article 1 folder

--- dataCollector.py
+++
@@ -1,8 +0,0 @@
-from populator import populate
-from pandas import DataFrame
-
-data = populate()
-
-# change csv output to desired file name
-df = DataFrame(data)
-df.to_csv('output.csv')
--- testPopulate.py
+++
@@ -1,6 +0,0 @@
-from populator import populate
-
-data = []
-populate(data)
-
-print(data)
